019a32888d93972720510deead680601e14b5227
Laisses <laisse@gmail.com>
2023-01-21 17:51:59 -0300

    src/routes.ts: import Express to type app

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import * as h from "./handlers.js";
+import { Express } from "express";
 
-export const routes = app => {
+
+export const routes = (app: Express) => {
     app.get("/health", h.health);
 
     app.get("/", h.getPlaces);
