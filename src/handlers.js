852ff9e19950a22907eec9a01ee616c39f20e9a0
Laisses <laisse@gmail.com>
2023-01-21 16:39:04 -0300

    src/handlers.js: fix the name of the repository function

--- src/handlers.js
+++ src/handlers.js
@@ -1,6 +1,6 @@
 import * as r from "./repositories.js";
 
 export const getPlaces = async (_req, res) => {
-    const places = await r.getPlaces();
+    const places = await r.selectPlaces();
     res.status(200).send(places.rows);
 };
