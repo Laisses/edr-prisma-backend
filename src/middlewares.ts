3a4e219e6ef8af455b62b328f1f1d3cd25b78005
Laisses <laisse@gmail.com>
2023-01-22 09:57:57 -0300

    src/middlewares.ts: create a id validation middleware

---
+++ src/middlewares.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import * as r from "./repositories.js";
+
+export const validateId = async (req: Request, res: Response, next) => {
+    const { id } = req.params;
+    const place = await r.selectPlaceById(Number(id));
+
+    if (!place.rows[0]) {
+        return res.status(404).send({message: "could not find specified place"});
+    }
+};
