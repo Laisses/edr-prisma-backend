949fecf7be557f77947f4ce52a53b4ed5cc4a66f
Laisses <laisse@gmail.com>
2023-01-22 10:00:30 -0300

    src/middlewares.ts: add NextFunction type to validadeId middleware

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,11 +1,13 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import * as r from "./repositories.js";
 
-export const validateId = async (req: Request, res: Response, next) => {
+export const validateId = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const place = await r.selectPlaceById(Number(id));
 
     if (!place.rows[0]) {
         return res.status(404).send({message: "could not find specified place"});
     }
+
+    next();
 };
